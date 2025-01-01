35669acaffc5ad72cfe5405c1da8c838fa0c5381
johnyowoy <john-7113113@hotmail.com>
2025-01-02 00:38:43 +0800

    modify docusaurus.config.js config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,15 +15,15 @@ const config = {
   favicon: 'img/myIcon.png',
 
   // Set the production url of your site here
-  url: 'https://johnyowoy.github.io',
+  url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Blog',
+  baseUrl: '/johnyowoy-website',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'johnyowoy', // Usually your GitHub org/user name.
-  projectName: 'Blog', // Usually your repo name.
+  projectName: 'johnyowoy-website', // Usually your repo name.
   deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
